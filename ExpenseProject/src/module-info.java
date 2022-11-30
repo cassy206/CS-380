468ce036d8138d0c15988fc3a689c4ab352f92ac
cassy206 <84590186+cassy206@users.noreply.github.com>
2022-11-29 23:38:09 -0800

    Delete ExpenseProject directory

--- ExpenseProject/src/module-info.java
+++
@@ -1,8 +0,0 @@
-module CS380_JavaFX_Project {
-	requires javafx.controls;
-	requires javafx.fxml;
-	requires javafx.base;
-	requires javafx.graphics;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
